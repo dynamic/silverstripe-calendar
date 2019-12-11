b860ad06463991f6719d28f68250775a93333422
Nic Horstmeier <nhorstmeier@dynamicagency.com>
2019-12-10 20:31:11 -0600

    ENAHNCEMENT Datetime to Date/Time data conversion task

---
+++ src/Task/DateTimeConversion.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Dynamic\Calendar\Task;
+
+use Dynamic\Calendar\Page\EventPage;
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\Versioned\Versioned;
+
+/**
+ * Class DateTimeConversion
+ * @package Dynamic\Calendar\Task
+ */
+class DateTimeConversion extends BuildTask
+{
+    /**
+     * @var string
+     */
+    protected $title = 'Calendar - Legacy Datetime Conversion Task';
+
+    /**
+     * @var string
+     */
+    private static $segment = 'calendar-datetime-conversion-task';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Convert Datetime data to separate Date and Time data';
+
+    /**
+     * @param \SilverStripe\Control\HTTPRequest $request
+     */
+    public function run($request)
+    {
+        $this->convertData();
+    }
+
+    /**
+     *
+     */
+    protected function convertData()
+    {
+        /** @var EventPage $event */
+        foreach ($this->yieldEvents() as $event) {
+            if ($event instanceof EventPage && $event->exists()) {
+                $isPublished = $event->isPublished();
+                $latestPublished = $event->isLatestVersion();
+
+                if ($event->StartDatetime && !$event->StartDate) {
+                    $startTimestamp = strtotime($event->StartDatetime);
+
+                    $event->StartDate = date('Y-m-d', $startTimestamp);
+                    $event->StartTime = date('H:i:s', $startTimestamp);
+                }
+
+                if ($event->EndDatetime && !$event->EndDate) {
+                    $endTimestamp = strtotime($event->EndDatetime);
+
+                    $event->EndDate = date('Y-m-d', $endTimestamp);
+                    $event->EndTime = date('H:i:s', $endTimestamp);
+                }
+
+                $event->writeToStage(Versioned::DRAFT);
+
+                if ($isPublished && $latestPublished) {
+                    $event->publishRecursive();
+                }
+            }
+        }
+    }
+
+    /**
+     * @return \Generator
+     */
+    protected function yieldEvents()
+    {
+        foreach (EventPage::get() as $event) {
+            yield $event;
+        }
+    }
+}

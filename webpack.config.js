d714d13c837e178e214957dc9902c424da41e87e
Jason Irish <jirish@dynamicagency.com>
2025-09-26 18:30:09 -0500

    Apply PHPCS auto-fixes

--- webpack.config.js
+++ webpack.config.js
@@ -4,94 +4,94 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
+    const isProduction = argv.mode === 'production';
 
-  return {
-    entry: {
-      calendar: './client/src/js/calendar.js'
-    },
-    output: {
-      path: path.resolve(__dirname, 'client/dist'),
-      filename: 'js/[name].bundle.js',
-      clean: true
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          }
+    return {
+        entry: {
+            calendar: './client/src/js/calendar.js'
         },
-        {
-          test: /\.scss$/,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+        output: {
+            path: path.resolve(__dirname, 'client/dist'),
+            filename: 'js/[name].bundle.js',
+            clean: true
+        },
+        module: {
+            rules: [
             {
-              loader: 'css-loader',
-              options: {
-                sourceMap: !isProduction
-              }
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             },
             {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: !isProduction,
-                sassOptions: {
-                  includePaths: (fs.existsSync(path.resolve(__dirname, 'node_modules')))
-                    ? [path.resolve(__dirname, 'node_modules')]
-                    : []
+                test: /\.scss$/,
+                use: [
+                isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: !isProduction
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: !isProduction,
+                        sassOptions: {
+                            includePaths: (fs.existsSync(path.resolve(__dirname, 'node_modules')))
+                            ? [path.resolve(__dirname, 'node_modules')]
+                            : []
+                        }
+                    }
                 }
-              }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[hash][ext][query]'
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[hash][ext][query]'
+                }
             }
-          ]
+            ]
         },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'images/[hash][ext][query]'
-          }
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'fonts/[hash][ext][query]'
-          }
-        }
-      ]
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: 'css/[name].bundle.css'
-      }),
+        plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].bundle.css'
+        }),
       new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development'),
-        __DEV__: JSON.stringify(!isProduction)
-      })
+            'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development'),
+            __DEV__: JSON.stringify(!isProduction)
+        })
     ],
     devtool: isProduction ? false : 'source-map',
     optimization: {
-      splitChunks: {
-        chunks: 'all',
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all'
-          }
+        splitChunks: {
+            chunks: 'all',
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\ / ] / ,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
         }
-      }
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'client/src')
-      }
-    }
-  };
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'client/src')
+            }
+        }
+    };
 };
